c4a7792be3772d97fc469387ccecc3a40d81ac3e
Konstantin <rojuty@gmail.com>
2024-04-19 16:51:34 +0700

    rearranged folders and files

--- Assets/src/Main.cs
+++ Assets/src/Main.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
 
 public class Main : MonoBehaviour{
+    public Player        Player;
+    public PlayerInput   PlayerInput;
     public EntityManager EntityManager;
     public TaskRunner    TaskRunner;
+    public EnemySpawner  SpawnerTask;
     
     private void Awake(){
         Singleton<EntityManager>.Create(EntityManager);
         Singleton<TaskRunner>.Create(TaskRunner);
+        Singleton<Player>.Create(Player);
     }
     
     private void Start(){
         EntityManager.BakeEntities();
+        
+        TaskRunner.StartTask(TaskGroupType.Gameplay, SpawnerTask);
     }
     
     private void Update(){
+        PlayerInput.Execute();
+        EntityManager.Execute();
         
+        TaskRunner.RunTaskGroup(TaskGroupType.Gameplay);
     }
 }
